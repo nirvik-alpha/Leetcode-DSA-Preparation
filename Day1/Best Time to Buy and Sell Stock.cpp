83817b227273c326998a26e09e0dc88f9dbc05dc
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2022-12-17 01:13:41 +0600

    Array Part-I problem solve

---
+++ Day1/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+       int minimum  = prices[0];
+        int  profit=0;
+        for(int i=1;i<prices.size();i++){
+            
+            minimum = min(minimum , prices[i]);
+            profit = max(profit , prices[i] - minimum);
+        }
+        
+        return profit;
+        
+    }
+};
---
+++ Day1/Maximum Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0;
+        int mx = nums[0];
+        for(int i=0;i<nums.size();i++)
+        {
+            if(sum<0)
+                sum=0;
+
+            sum+=nums[i];
+            mx = max(mx , sum);
+        }
+
+    return  mx;
+    }
+};
