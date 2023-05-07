ba18dccdfb5dd0fe717d8de341675443e3bb5d15
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2023-05-07 22:32:47 +0600

    Add files via upload

---
+++ Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+
+    void inorder(TreeNode* root , vector<int>&ans)
+    {
+        if(root==NULL)          // left --  root -- right
+        return ;
+
+        inorder(root->left , ans);
+        ans.push_back(root->val);
+        inorder(root->right , ans);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int>ans;
+        inorder(root,ans);
+        return ans;
+    }
+};
