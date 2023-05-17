72502d0dc583d4f0bcc8831b834c463f5f394d09
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2023-05-17 13:02:34 +0600

    Linked List

---
+++ Odd Even Linked List.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+      if(!head || !head->next) return head;
+        ListNode* odd = head,*even = head->next;
+        ListNode* even_ = even;
+        while(odd->next && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_;
+        return head;
+
+    }
+};
