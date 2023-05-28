7732eafb2bec4ab816d59fe9b0b7f1e27f5f0bd6
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2023-05-28 13:03:17 +0600

    Add files via upload

---
+++ Search in a Binary Search Tree.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root==NULL)
+        return root;
+
+        bool left ;
+        bool right ;
+
+        if(root->val==val)
+        return root;
+
+        if(root->val > val)
+        return  searchBST(root->left , val);
+
+        else
+        return  searchBST(root->right , val);
+
+    
+    }
+};
